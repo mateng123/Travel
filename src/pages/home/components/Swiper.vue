62d304a2ef966173ffdaf3887bb3efc810f370fc
mateng123 <mateng689@163.com>
2019-11-19 19:26:53 +0800

    加入滚动图片（去哪儿网图片没有url了，我用携程的替代了，但携程的图片宽度不够，所以看起来有些怪）

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swapper-img" :src="item.imgUrl"/>
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://dimg04.c-ctrip.com/images/zg0t1800000152y0m2769.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://dimg04.c-ctrip.com/images/zg0j170000011chb42B7F.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swapper-img
+      width: 100%
+</style>
