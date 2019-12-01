d1372f91ef363e557f6dbdcd337f09bf47ed8f0d
mateng123 <mateng689@163.com>
2019-12-01 18:52:24 +0800

    数据改用json文件

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,44 +1,47 @@
 <template>
-  <div class="wrapper">
-  <swiper :options="swiperOption">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
-      <img class="swapper-img" :src="item.imgUrl"/>
-    </swiper-slide>
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
-  </div>
+    <div class="wrapper">
+        <swiper :options="swiperOption" v-if="showSwiper">
+            <swiper-slide v-for="item of list" :key="item.id">
+                <img class="swapper-img" :src="item.imgUrl"/>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'https://imgs.qunarzz.com/vc/de/3d/1c/2ea7755853fef62ce549153026.jpg_92.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/abe3418971067bd655fb2794d6e2eeec.jpg'
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
 </script>
 
 <style scoped lang="stylus">
-  .wrapper >>> .swiper-pagination-bullet-active
-    background #fff
-  .wrapper
-    overflow: hidden
-    width: 100%
-    height: 0
-    padding-bottom: 31.25%
-    background: #eee
-    .swapper-img
-      width: 100%
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff
+
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        background: #eee
+
+        .swapper-img
+            width: 100%
 </style>
